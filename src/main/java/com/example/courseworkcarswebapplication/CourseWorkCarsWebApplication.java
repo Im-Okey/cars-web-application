cfe0471e518d81876b34a35fded6fc964452a418
Arseny <arseny.butko771@gmail.com>
2023-04-12 22:54:11 +0300

    start project

---
+++ src/main/java/com/example/courseworkcarswebapplication/CourseWorkCarsWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.courseworkcarswebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkCarsWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkCarsWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/courseworkcarswebapplication/CourseWorkCarsWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.courseworkcarswebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkCarsWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
