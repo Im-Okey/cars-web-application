dd3b0566aea9c6a6f1ccc99273b34e6982bd2218
Arseny <arseny.butko771@gmail.com>
2023-04-13 21:27:29 +0300

    image-test-failed

---
+++ src/main/java/com/example/courseworkcarswebapplication/controllers/ImageController.java
@@ -0,0 +1,2 @@
+package com.example.courseworkcarswebapplication.controllers;public class ImageController {
+}
---
+++ src/main/java/com/example/courseworkcarswebapplication/repositories/ImageRepository.java
@@ -0,0 +1,2 @@
+package com.example.courseworkcarswebapplication.repositories;public interface ImageRepository {
+}
